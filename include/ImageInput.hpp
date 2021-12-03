b79ac7963c532175ba694e50cd4b214a4a7c087c
atora <yuuya67200118@gmail.com>
2021-12-03 15:08:21 +0000

    rinkaku add

---
+++ include/ImageInput.hpp
@@ -0,0 +1,39 @@
+#pragma once
+//opencvのライブラリ取得
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <map>
+#include <list>
+using namespace std;
+typedef struct CONTOUR_TAG {	
+	int x;
+    int y;
+}CONTOURData;
+
+//サイズ変更を行いたい数値に変更できる
+#define BASICWIDTH 100
+#define BASICHEIGHT 100
+class InputImage
+{
+    public:
+        //コンストラクタ
+        InputImage();
+        //デストラクタ
+        ~InputImage();
+        //画像読み込み
+        int Input(char*,IplImage**);
+        //画像サイズ変換
+        //画像のサイズを指定したサイズ二合わせて拡大縮小を行う
+        int ImageSizeChange(IplImage*,IplImage**);
+        //画像の輪郭抽出
+        //IpImege型の画像を送信することで変換した値を返す
+        int ImageContour(IplImage*);
+        
+    private:
+        std::map<int,std::list<CONTOURData>> mp;
+        //輪郭最外層の頂点取得
+        int NextContour(CvSeq *Contour,int Contourcnt);
+
+};
---
+++ src/ImageInput.cpp
@@ -0,0 +1,87 @@
+#include "./ImageInput.hpp"
+//コンストラクタ
+InputImage::InputImage(){}
+//デストラクタ
+InputImage::~InputImage(){}
+//画像読み込み
+int InputImage::Input(char* _FilePath,IplImage** inputfile){
+    if(_FilePath==NULL){
+        return -2;
+    }
+    //画像データの読込（グレースケールで読込）
+    //opencv上のエラーで処理の停止を防ぐ
+    try{
+        *inputfile = cvLoadImage((const char*)_FilePath, CV_LOAD_IMAGE_GRAYSCALE);
+    }catch(cv::Exception& e){
+        printf("cvLoadImage:opencv上でエラーが発生");
+        return -1;
+    }
+    return 0;
+}
+
+//画像のサイズ変更
+int InputImage::ImageSizeChange(IplImage* _inputimage,IplImage** _outputimage){
+    float rewidth=_inputimage->width;
+    float reheight=_inputimage->height;
+
+    rewidth=BASICWIDTH/rewidth;
+    reheight=BASICHEIGHT/reheight;
+    //opencv上のエラーで処理の停止を防ぐ
+    try{
+        *_outputimage = cvCreateImage(cvSize((int)_inputimage->width*rewidth,(int)_inputimage->height*reheight),_inputimage->depth,1);
+        cvResize(_inputimage, *_outputimage, CV_INTER_LINEAR);
+    }catch(cv::Exception& e){
+        printf("cvCreateImage,cvResize:opencv上でエラーが発生");
+        return -1;
+    }
+
+    return 0;
+}
+
+//輪郭抽出
+int InputImage::ImageContour(IplImage* _inputimage){
+    if(_inputimage == NULL){
+        return -2;
+    }
+    CvSeq *firstcontours = NULL;
+    CvMemStorage *storage = cvCreateMemStorage (0);
+    //輪郭データ取得
+    try{
+	cvThreshold (_inputimage, _inputimage, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+    }catch(cv::Exception& e){
+        printf("cvThreshold:opencv上でエラーが発生");
+        return -1;
+    }
+    int allcontourcnt = cvFindContours(_inputimage,storage,&firstcontours,sizeof (CvContour),CV_RETR_LIST,CV_CHAIN_APPROX_SIMPLE);
+
+    if (firstcontours != NULL){
+	//輪郭の描画
+	NextContour( firstcontours, 1);
+    }
+    //メモリストレージの解放
+	cvReleaseMemStorage (&storage);
+}
+
+//輪郭の最外層
+int InputImage::NextContour(CvSeq *Contour,int Contourcnt)
+{
+    std::list<CONTOURData> listtemp;
+    CONTOURData structdata;
+	//輪郭を構成する頂点座標を取得
+	for ( int index = 0; index < Contour->total;index++){
+	    CvPoint *point = CV_GET_SEQ_ELEM (CvPoint, Contour,index);
+	    structdata.x=point->x;
+	    structdata.y=point->y;
+	    printf("%d,%d\n",point->x,point->y);
+	    listtemp.push_back(structdata);
+	}
+	mp[Contourcnt]=listtemp;
+	if (Contour->h_next != NULL){
+	    //次の輪郭がある場合は次の輪郭を描画
+	    NextContour(Contour->h_next, Contourcnt+1);
+	}
+    return 0;
+
+}
+
+
