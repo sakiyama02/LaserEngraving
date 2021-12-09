5dad04d1310d50f373702662156ee069f6b20472
sakiyama <sakiyama.hayato02@gmail.com>
2021-12-09 15:44:19 +0900

    割り込み処理テスト

--- Test.cpp
+++ Test.cpp
@@ -5,44 +5,57 @@
 #define SW_PORT13 13
 
 void interrupt5(){
-	delay(10);
-	printf("hello interrupt5\n");
-	while( digitalRead(SW_PORT5) == 0){}
+	delay(150);
+	if(digitalRead(SW_PORT5)==1){
+		printf("hello interrupt5\n");
+		printf("status=%d\n",digitalRead(SW_PORT5));
+	}else{
 
-	printf("Bye interrupt5\n");
-	delay(10);
+	}
 }
-void interrupt6(){
-	delay(10);
-	printf("hello interrupt6\n");
-	while( digitalRead(SW_PORT6) == 0){}
 
-	printf("Bye interrupt6\n");
-	delay(10);
+void interrupt6(){
+	delay(150);
+	if(digitalRead(SW_PORT6)==1){
+		printf("hello interrupt6\n");
+		printf("status=%d\n",digitalRead(SW_PORT6));
+	}else{
+
+	}
 }
 void interrupt13(){
-	delay(10);
-	printf("hello interrupt13\n");
-	while( digitalRead(SW_PORT13) == 0){}
+	delay(150);
+	if(digitalRead(SW_PORT13)==1){
+		printf("hello interrupt13\n");
+		printf("status=%d\n",digitalRead(SW_PORT13));
+	}else{
 
-	printf("Bye interrupt13\n");
-	delay(10);
+	}
 }
+
+class test{
+	public:
+	test(){
+		
+		pinMode(SW_PORT5,INPUT);
+		pinMode(SW_PORT6,INPUT);
+		pinMode(SW_PORT13,INPUT);
+
+		pullUpDnControl(SW_PORT5,PUD_DOWN);
+		pullUpDnControl(SW_PORT6,PUD_DOWN);
+		pullUpDnControl(SW_PORT13,PUD_DOWN);
+
+		wiringPiISR(SW_PORT5,INT_EDGE_BOTH,&interrupt5);
+		wiringPiISR(SW_PORT6,INT_EDGE_RISING,&interrupt6);
+		wiringPiISR(SW_PORT13,INT_EDGE_RISING,&interrupt13);
+	}
+	~test(){}
+
+};
 
 int main(){
 	if(wiringPiSetupGpio() < 0) return 1;
-	pinMode(SW_PORT5,INPUT);
-	pinMode(SW_PORT6,INPUT);
-	pinMode(SW_PORT13,INPUT);
-
-	pullUpDnControl(SW_PORT5,PUD_DOWN);
-	pullUpDnControl(SW_PORT6,PUD_DOWN);
-	pullUpDnControl(SW_PORT13,PUD_DOWN);
-
-	wiringPiISR(SW_PORT5,INT_EDGE_FALLING,interrupt5);
-	wiringPiISR(SW_PORT6,INT_EDGE_FALLING,interrupt5);
-	wiringPiISR(SW_PORT13,INT_EDGE_FALLING,interrupt5);
-
+	test a;
 
 	while(1){
 		delay(100);
