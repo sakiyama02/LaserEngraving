2bcce00184c6c7cc1076d9736d297f2d75b61a6a
sakiyama <sakiyama.hayato02@gmail.com>
2022-01-28 13:51:11 +0900

    リファクタリング

--- src/ArmControl.cpp
+++ src/ArmControl.cpp
@@ -1,20 +1,49 @@
+/* ------------------------------------------------------------------------- */
+/* ArmControl.cpp															 */
+/*																			 */
+/*																			 */
+/* ------------------------------------------------------------------------- */
+/* ------------------------------------------------------------------------- */
+/* 番号		更新履歴								日付		氏名		 */
+/* ------------------------------------------------------------------------- */
+/* 000000	新規作成								2022/01/27	崎山　勇人	 */
+/* ------------------------------------------------------------------------- */
+
+/* ------------------------------------------------------------------------- */
+/* includeファイル															 */
+/* ------------------------------------------------------------------------- */
 #include "../include/ArmControl.hpp"
 
-
+/* ------------------------------------------------------------------------- */
+/* 関数名		： ArmControl												 */
+/* 機能名		： コンストラクタ											 */
+/* 機能概要		： メンバー変数の初期化										 */
+/* 引数			： なし														 */
+/* 戻り値		： 															 */
+/* 作成日		： 2022/02/27		 崎山　勇人		 新規作成			 	 */
+/* ------------------------------------------------------------------------- */
 ArmControl::ArmControl()
 {
+	BackMotor  &bMotor = BackMotor::getInstance();
+	FrontMotor &fMotor = FrontMotor::getInstance();
+	/* 初期化 */
 	length.l1 = 100;
 	length.l2 = 100;
 	degree.deg1 = 0;
 	degree.deg2 = 0;
-	BackMotor &bMotor = BackMotor::getInstance();
-	FrontMotor &fMotor = FrontMotor::getInstance();
 	bMotor.init();
 	fMotor.init();
 }
-
 ArmControl::~ArmControl() {}
 
+/* ------------------------------------------------------------------------- */
+/* 関数名		： stop														 */
+/* 機能名		： 停止														 */
+/* 機能概要		： 両方のモータを止める										 */
+/* 引数			： なし														 */
+/* 戻り値		： int				:0				:正常終了				 */
+/* 作成日		： 2022/02/27		 崎山　勇人		 新規作成			 	 */
+/* ------------------------------------------------------------------------- */
 int ArmControl::stop()
 {
 	BackMotor &bMotor = BackMotor::getInstance();
@@ -25,15 +54,21 @@ int ArmControl::stop()
 	return 1;
 }
 
-int ArmControl::changeSpeed(char speed)
+
+int ArmControl::init(int dir,char select)
 {
 	FrontMotor &fMotor = FrontMotor::getInstance();
-	BackMotor &bMotor = BackMotor::getInstance();
+	BackMotor  &bMotor = BackMotor::getInstance();
 
-	fMotor.changeSpeed(speed);
-	bMotor.changeSpeed(speed);
-
-	return 0;
+	/* selectの値によって駆動するモータを選ぶ */
+	if( select == FRONT )
+	{
+		fMotor.run( 1, dir );
+	} else {
+		bMotor.run( 1, dir );
+	}
+	delay( 2 );
+	return SYS_OK;
 }
 
 int ArmControl::frontinit(int dir)
@@ -51,40 +86,43 @@ int ArmControl::backinit(int dir)
 	return 1;
 }
 
+/* ------------------------------------------------------------------------- */
+/* 関数名		： run														 */
+/* 機能名		： 実行														 */
+/* 機能概要		： 逆運動計算を行い各角度でモータを動かす						 */
+/* 引数			： なし														 */
+/* 戻り値		： int				:0				:正常終了				 */
+/* 作成日		： 2022/02/27		 崎山　勇人		 新規作成			 	 */
+/* ------------------------------------------------------------------------- */
 int ArmControl::run(double x, double y)
 {
-	BackMotor &bMotor = BackMotor::getInstance();
+	BackMotor &bMotor  = BackMotor::getInstance();
 	FrontMotor &fMotor = FrontMotor::getInstance();
 
-	POS pos;
+	POSITION pos;
 	pos.x = x;
 	pos.y = y;
 	// 逆運動学で座標から各ラジアンを取得
-	TH t = calc(pos);
+	THETA t = calc(pos);
 	// ラジアンから角度に変換
-	double degree0 = t.th0 * 180.0 / PI;
-	double degree1 = t.th1 * 180.0 / PI;
-	// char bStatus[2]={0,0};
-	// char fStatus[2]={0,0};
+	double degree0 = t.th1 * 180.0 / PI;
+	double degree1 = t.th2 * 180.0 / PI;
 	int step1 = 0;
 	int step2 = 0;
 	int dir1 = 0;
 	int dir2 = 0;
 	int max = 0;
 
-	// printf("%f,%f\n",degree0,degree1);
-
 	// 角度からstep数を取得
-	step1 = (int)round((degree0 - deg.deg0) / ONE_MICRO_STEP);
-	step2 = (int)round((degree1 - deg.deg1) / ONE_MICRO_STEP);
+	step1 = (int)round((degree0 - degree.deg1) / ONE_MICRO_STEP);
+	step2 = (int)round((degree1 - degree.deg2) / ONE_MICRO_STEP);
 
-	// printf("%d    %d\n",step1,step2);
-	(degree0 - deg.deg0 > 0) ? dir1 = 1 : dir1 = 0;
-	(degree1 - deg.deg1 > 0) ? dir2 = 1 : dir2 = 0;
+	(degree0 - degree.deg1 > 0) ? dir1 = 1 : dir1 = 0;
+	(degree1 - degree.deg2 > 0) ? dir2 = 1 : dir2 = 0;
 	(step1 < 0) ? step1 = -step1 : step1;
 	(step2 < 0) ? step2 = -step2 : step2;
-	deg.deg0 = degree0;
-	deg.deg1 = degree1;
+	degree.deg1 = degree0;
+	degree.deg2 = degree1;
 	// 最大値を取得
 	max = step1;
 	(max < step2) ? max = step2 : max;
@@ -102,22 +140,29 @@ int ArmControl::run(double x, double y)
 		delay(2);
 	}
 
-	// printf("MotorStop\n");
 	return 1;
 }
 
-TH ArmControl::calc(POS pos)
+/* ------------------------------------------------------------------------- */
+/* 関数名		： calc														 */
+/* 機能名		： 逆運動													 */
+/* 機能概要		： 逆運動計算を行う											 */
+/* 引数			： POSITION			:				:座標					 */
+/* 戻り値		： THETA			:0				:正常終了				 */
+/* 作成日		： 2022/02/27		 崎山　勇人		 新規作成			 	 */
+/* ------------------------------------------------------------------------- */
+THETA ArmControl::calc(POSITION pos)
 {
 	double x = pos.x;
 	double y = pos.y;
-	double l0 = length.l0;
 	double l1 = length.l1;
+	double l2 = length.l2;
 
 	double norm = sqrt((pos.x * pos.x) + (pos.y * pos.y));
 
-	TH T;
-	T.th0 = atan2(y, x) - acos(((x * x) + (y * y) + (l0 * l0) - (l1 * l1)) / (2 * l0 * norm));
-	T.th1 = -T.th0 + atan2((y - l0 * sin(T.th0)), (x - l0 * cos(T.th0)));
+	THETA T;
+	T.th1 = atan2(y, x) - acos(((x * x) + (y * y) + (l1 * l1) - (l1 * l2)) / (2 * l1 * norm));
+	T.th2 = -T.th1 + atan2((y - l1 * sin(T.th1)), (x - l1 * cos(T.th1)));
 
 	return T;
 }
